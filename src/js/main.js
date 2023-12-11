7a8cef7a63141f9f9ea3731cbff249bc8cd2fb3f
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2023-12-11 14:46:03 +0300

    fix: change logic in the hint block

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,56 @@ import japaneseNumbers from "./japaneseNumbers.js";
 
 let name = "";
 let number = Math.round(Math.random() * 100);
-let guesses = 0;
+let attemps = 0;
 
 const output = document.querySelector("#output");
 const clue = document.querySelector("#clue");
 const input = document.querySelector("#clue input");
+const clueNumber = document.querySelector("#clueNumber");
+const inputNumber = document.querySelector("#clueNumber input");
+const selectNumber = document.querySelector("#clueNumber select");
+const hint = document.querySelector("#hint");
+const notice = document.querySelector("#notice");
+const button = document.querySelector(".button");
+
+for (let i = 1; i <= 500; i++) {
+  let option = document.createElement("option");
+  option.value = i;
+  option.text = i;
+  selectNumber.appendChild(option);
+}
 
 input.focus();
 
-const handleSubmit = (evt) => {
+const handleSubmitInputName = (evt) => {
   evt.preventDefault();
 
   processInput(input.value);
+  inputNumber.focus();
 
   input.value = "";
 };
 
-const printMessage = (message) => {
+const handleSubmitInputNumber = (evt) => {
+  evt.preventDefault();
+
+  processInputNumber(inputNumber.value);
+  inputNumber.value = "";
+};
+
+const printMessage = (message, japaneseNumbers) => {
   let li = document.createElement("li");
+  let span = document.createElement("span");
   li.style.color = "white";
+  span.style.color = "rgb(252, 144, 252)";
   li.style.fontSize = 18 + "px";
+  span.style.fontSize = 24 + "px";
   li.style.paddingTop = "5px";
 
   li.textContent = message;
+  span.textContent = japaneseNumbers;
   output.appendChild(li);
+  output.appendChild(span);
 };
 
 printMessage("Введите имя ㊔");
@@ -43,17 +69,22 @@ const processInput = (input) => {
     name = input;
     clearOutput();
     printMessage(`${input}, я загадал число от 0 до 100. Попробуй отгадать его за минимальное количество попыток. 
-     После каждой попытки я буду говорить "мало", "много" или "верно".`);
+     После каждой попытки я буду говорить "мало", "много" или "верно". Как придумаете число - нажимайте Enter.`);
+
+    clue.style.display = "none";
+    clueNumber.style.display = "flex";
 
     return;
   }
+};
 
-  printMessage(input);
+const processInputNumber = (inputNumber) => {
+  printMessage(inputNumber);
 
-  let guess = Number.parseInt(input);
+  let guess = +inputNumber;
 
   if (Number.isNaN(guess)) return;
-  guesses++;
+  attemps++;
 
   if (guess > number) {
     printMessage("Много, попробуй еще раз");
@@ -61,12 +92,45 @@ const processInput = (input) => {
     printMessage("Мало, попробуй еще раз");
   } else if (guess === number) {
     printMessage(
-      `Верно! Это число ${guess} или как бы сказали самураи: ${japaneseNumbers[number]}`
+      `Верно! Это число ${guess} или как бы сказали самураи: `,
+      japaneseNumbers[number]
     );
-    printMessage(`Количество попыток ${guesses}`);
+    printMessage(`Количество попыток ${attemps}`);
 
-    clue.remove();
+    clueNumber.remove();
+  }
+
+  // Если количество попыток достигло трех
+  if (attemps % 3 === 0) {
+    // Показываем подсказку о четности или нечетности числа
+    hint.style.display = "block";
+
+    if (guess % 2 === 0) {
+      hint.innerText = "Число нечетное";
+    } else {
+      hint.innerText = "Число четное";
+    }
+  } else {
+    // Скрываем подсказку
+    hint.style.display = "none";
+  }
+
+  if (guess < 1 || guess > 100) {
+    notice.textContent =
+      "Число должно быть в диапазоне от 1 до 100 включительно";
+  } else {
+    notice.textContent = "";
   }
 };
 
-clue.addEventListener("submit", handleSubmit);
+const reloadPage = () => {
+  location.reload();
+};
+
+clue.addEventListener("submit", handleSubmitInputName);
+clueNumber.addEventListener("submit", handleSubmitInputNumber);
+button.addEventListener("click", reloadPage);
+selectNumber.addEventListener("change", () => {
+  inputNumber.value = selectNumber.value;
+  inputNumber.focus();
+});
